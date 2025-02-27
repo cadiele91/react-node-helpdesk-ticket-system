5e941de90916eb0f067660cae0bd91a0a3c9040e
Christopher Adiele <christopherchiagoziem.adiele@student.griffith.ie>
2025-02-27 08:16:56 +0000

    Created Ticket Form component

--- node-express-app/src/App.js
+++ node-express-app/src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import { fetchAllTickets } from './service/ticketService';
+import "./App.css"
+import { createTicket, fetchAllTickets } from './service/ticketService';
+import TicketForm from "./component/TicketForm/TicketForm";
 
 function App() {
   const [tickets, setTickets] = useState([]);
+  const [currentTicket, setCurrentTicket] = useState({});
 
   // Define the function inside the component
   const getAllTickets = async () => {
@@ -11,6 +14,31 @@ function App() {
     setTickets(ticketsData);
   };
 
+  const sendCreateRequest = async (id,
+    summary,
+    priority,
+    status,
+    create_date,
+    update_date
+  ) => { 
+    const createdTicket = await createTicket({
+    id,
+    summary,
+    priority,
+    status,
+    create_date,
+    update_date
+    })
+    
+    if(!createdTicket){
+      return;
+    }
+
+    getAllTickets();
+    setCurrentTicket(createdTicket);
+  };
+
+
   // Use useEffect to call it when the component mounts
   useEffect(() => {
     getAllTickets();
@@ -18,19 +46,37 @@ function App() {
 
   return (
     <div className="App">
+      <TicketForm
+          id={currentTicket.id}
+          summary={currentTicket.summary}
+          priority={currentTicket.priority}
+          status={currentTicket.status}
+          createDate={
+            currentTicket.create_date
+              ? new Date(currentTicket.create_date)
+              : new Date()
+          }
+          updateDate={
+            currentTicket.update_date
+              ? new Date(currentTicket.update_date)
+              : new Date()
+          }
+          readonly={false}
+          onSubmit={sendCreateRequest}
+        />
       <h1>Tickets</h1>
       {tickets.length === 0 ? (
         <p>No tickets found</p>
       ) : (
         tickets.map((ticket, index) => (
-          <div key={index}>
+          <div key={index} style={{border: '1px solid black'}} >
             <p>{ticket.id}</p>
             <p>{ticket.summary}</p>
             <p>{ticket.priority}</p>
             <p>{ticket.status}</p>
             <p>{ticket.create_date}</p>
             <p>{ticket.update_date}</p>
-          </div>
+          </div> 
         ))
       )}
     </div>
--- node-express-app/src/service/ticketService.js
+++ node-express-app/src/service/ticketService.js
@@ -1,4 +1,67 @@
+
+
 export const fetchAllTickets = async () => {
     const rawResponse = await fetch("http://localhost:3001/all");
+    return await rawResponse.json();
+  };
+
+  export const fetchTicket = async (id) => {
+    const rawResponse = await fetch(`http://localhost:3001/ticket/${id}`);
+    return await rawResponse.json();
+  };
+  
+export const createTicket = async (ticket) => {
+    const rawResponse = await fetch(`http://localhost:3001/ticket`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(ticket),
+    });
+  
+    if (rawResponse.status !== 200) {
+      console.error("Could not create ticket.");
+      return null;
+    }
+  
+    return await rawResponse.json();
+  };
+  
+  export const updateTicket = async (id, ticket) => {
+    if (ticket.id) {
+      delete ticket.id;
+    }
+    const rawResponse = await fetch(`http://localhost:3001/ticket/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(ticket),
+    });
+  
+    if (rawResponse.status !== 200) {
+      console.error("Could not update ticket.");
+      return null;
+    }
+  
+    return await rawResponse.json();
+  };
+  
+  export const deleteTicket = async (id) => {
+    const rawResponse = await fetch(`http://localhost:3001/ticket/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+  
+    if (rawResponse.status !== 200) {
+      console.error("Could not delete ticket.");
+      return null;
+    }
+  
     return await rawResponse.json();
   };
